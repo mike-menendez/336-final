2be009d7234e3374a2611a3675fb0bd83ff3131c
Mike Menendez <mmenendez@csumb.edu>
2019-12-11 11:33:11 -0800

    Backend loging

--- routes/admin.js
+++ routes/admin.js
@@ -80,30 +80,30 @@ router.post('/create', function(req, res, next) {
     var q = "";
     console.log("REQ: ", req);
     console.log("BODY: ", req.body);
-    if (req.body[0] == "items") {
-        q = "INSERT INTO items (p_name, cat, price, img) VALUES (\"" + req.body.p_name + "\", \"" + req.body.cat + "\", \"" + req.body.price + "\", \"" + req.body.img + "\")";
-    } else if (req.body[0] == "users") {
-        q = "INSERT INTO users (uname, pass) VALUES (\"" + req.body.uname + "\", \"" + req.body.pass + "\")";
-    } else {
-        q = "INSERT INTO order_hist (item_id, quantity) VALUES (\"" + req.body.id + "\", \"" + req.body.quant + "\")";
-    }
-    connection.query(q, function(err, results) {
-        if (err) {
-            console.log(connection.state);
-            console.log(err);
-            throw err;
-        }
-        connection.end();
-        try {
-            var x = JSON.parse(JSON.stringify(results));
+    // if (req.body[0] == "items") {
+    //     q = "INSERT INTO items (p_name, cat, price, img) VALUES (\"" + req.body.p_name + "\", \"" + req.body.cat + "\", \"" + req.body.price + "\", \"" + req.body.img + "\")";
+    // } else if (req.body[0] == "users") {
+    //     q = "INSERT INTO users (uname, pass) VALUES (\"" + req.body.uname + "\", \"" + req.body.pass + "\")";
+    // } else {
+    //     q = "INSERT INTO order_hist (item_id, quantity) VALUES (\"" + req.body.id + "\", \"" + req.body.quant + "\")";
+    // }
+    // connection.query(q, function(err, results) {
+    //     if (err) {
+    //         console.log(connection.state);
+    //         console.log(err);
+    //         throw err;
+    //     }
+    //     connection.end();
+    //     try {
+    //         var x = JSON.parse(JSON.stringify(results));
 
-            res.json(x).status(200);
+    //         res.json(x).status(200);
 
-        } catch (error) {
-            console.log("catch area");
-            res.json({ auth: "false" }).status(200);
-        }
-    });
+    //     } catch (error) {
+    //         console.log("catch area");
+    //         res.json({ auth: "false" }).status(200);
+    //     }
+    // });
 });
 
 module.exports = router;
