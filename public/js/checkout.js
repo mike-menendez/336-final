95ad3171bcd47d4dc4baf26514a96dadc39a05b2
Sam Westigard <sjwestigard@gmail.com>
2019-12-12 01:45:28 -0800

    ..

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
   $(".cart").text(function() { return 0; });
   sessionStorage.setItem("icount", 0);
   sessionStorage.setitem("cart": null);
+  console.log('heeeey');
 });
