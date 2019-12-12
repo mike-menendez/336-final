c83d91e1a28d5716307a6425cbbff3b8c08cc2d7
Sam Westigard <sjwestigard@gmail.com>
2019-12-12 01:10:58 -0800

    ..

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,112 +1,51 @@
 // Set cart value and auth status
 
 $(document).ready(function() {
-  //   var cart = sessionStorage.getItem("cart");
-  // btnid = this.id;
-  // split = btnid.split("-");
-  // if (cart == null) {
-  //     cart = new Object;
-  //     cart[split[0]] = [1, split[1]];
-  // }
-  // else {
-  //     cart = JSON.parse(cart);
-  //     if (split[0] in cart) {
-  //         cart[split[0]][0] = cart[split[0]][0] + 1;
-  //     }
-  //     else {
-  //         cart[split[0]] = [1, split[1]];
-  //     }
-  // }
-  // sessionStorage.setItem("cart", JSON.stringify(cart));  
-
   let cart = sessionStorage.getItem("cart");
   if (cart != null) {
     cart = JSON.parse(cart);
     console.log(cart);
   }
-    // var itm = sessionStorage.getItem("icount");
-    // if (itm == null) {
-    //     itm = 0;
-    // } else if (itm > 9) {
-    //     itm = "9+";
-    // }
-    // $(".cart").text(function() { return itm; });
-
-    // var auth = sessionStorage.getItem("auth");
-
-    // if (auth == "true") {
-    //     $(".sin").text(function() { return "Sign Out"; });
-    //     $(".sin").attr("href", "/");
-    //     $(".sin").on("click", function() {
-    //         sessionStorage.removeItem("auth");
-    //         Swal.fire({
-    //             title: "You have been signed out\nTaking you home!",
-    //             icon: 'success',
-    //             timer: 2000,
-    //             timerProgressBar: true,
-    //             onBeforeOpen: () => {
-    //                 Swal.showLoading();
-    //                 timerInterval = setInterval(() => {
-    //                     Swal.getContent().querySelector('b')
-    //                         .textContent = Swal.getTimerLeft();
-    //                 }, 100);
-    //             },
-    //             onClose: () => {
-    //                 clearInterval(timerInterval);
-    //             }
-    //         }).then(function() {
-    //             setTimeout(() => { window.location.replace("https://final-cst336-mmenendez.herokuapp.com/"); }, 2000);
-    //         });
-    //     });
-    // } else {
-    //     $(".ap").remove();
-    // }
-
-    // $(".lbtn").on("click", function() {
-    //     var username = $(".uname").val();
-    //     console.log("Uname:", username);
-    //     var password = $(".pass").val();
-    //     console.log("Pass:", password);
-
-    //     $.ajax({
-    //         url: "https://final-cst336-mmenendez.herokuapp.com/auth",
-    //         type: "POST",
-    //         data: { username: username, password: password },
-    //         success: function(res) {
-    //             console.log(res.auth);
-    //             var x;
-    //             if (res.auth == true) {
-    //                 x = {
-    //                     title: "Login Success!",
-    //                     icon: 'success',
-    //                     showConfirmButton: true,
-    //                     confirmButtonText: 'Take Me Home'
-    //                 };
-    //                 sessionStorage.setItem("auth", "true");
-    //             } else {
-    //                 x = {
-    //                     title: "Login Failure",
-    //                     text: 'Invalid Username or Password',
-    //                     icon: 'error',
-    //                     showConfirmButton: true,
-    //                     confirmButtonText: 'Ok'
-    //                 };
-    //                 delete sessionStorage.auth;
-    //             }
-    //             Swal.fire(x).then(function() {
-    //                 if (x.icon == 'error') {
-    //                     window.location.reload();
-    //                 } else {
-    //                     window.location.replace("https://final-cst336-mmenendez.herokuapp.com/");
-    //                 }
-    //             });
-    //         }
-    //     });
-    // });
-
-    // // Search, requires preforming the query (handled in the router via POST), appending the HTML in a card like BreweryAPI
-    // $(".search").on("click", function() {
-
-    // });
-
+  cartPage = "";
+  
 });
+
+    // <div class="card container shadow p-5 justify-content-center mt-5">
+    //     <h1 class="text-center">Cart</h1>
+    //     <hr>
+    //     <div class="pb-5">
+    //         <div class="container">
+    //           <div class="row">
+    //             <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+    //               <div class="table-responsive">
+    //                 <table class="table">
+    //                   <thead>
+    //                     <tr>
+    //                       <th scope="col" class="border-0 bg-light">
+    //                         <div class="p-2 px-3 text-uppercase">Product</div>
+    //                       </th>
+    //                       <th scope="col" class="border-0 bg-light">
+    //                         <div class="py-2 text-uppercase">Price</div>
+    //                       </th>
+    //                       <th scope="col" class="border-0 bg-light">
+    //                         <div class="py-2 text-uppercase">Quantity</div>
+    //                       </th>
+    //                     </tr>
+    //                   </thead>
+    //                   <tbody>
+    //                     <tr>
+    //                       <th scope="row" class="border-0">
+    //                         <div class="p-2">
+    //                           <div class="ml-3 d-inline-block align-middle">
+    //                             <h5 class="mb-0">Timex Unisex Originals</h5>
+    //                           </div>
+    //                         </div>
+    //                       </th>
+    //                       <td class="border-0 align-middle"><strong>$79.00</strong></td>
+    //                       <td class="border-0 align-middle"><strong>3</strong></td>
+    //                     </tr>
+    //                   </tbody>
+    //                 </table>
+    //               </div>
+    //             </div>
+    //           </div>
